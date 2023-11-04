4f0793a8be326761235e3cde4b82f4233389017b
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-03 22:22:03 -0300

    delete folders not in use

--- src/app/(not-log)/restapi/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function ApiPage() {
-  return (
-    <div>
-      <h1>Api</h1>
-    </div>
-  )
-}
--- src/app/api/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Api () {
-  return (
-    <div> 
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet voluptatibus veniam, dolore totam debitis officiis maiores odit laboriosam sequi harum repellat illum ex corporis, impedit placeat. Tenetur tempore odit et!</p>
-       </div>
-  )
-}
-
