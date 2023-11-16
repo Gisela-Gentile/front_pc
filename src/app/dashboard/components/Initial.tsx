014b5bfdd732e38117f8321cc3ca8f36476cc1f5
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-16 20:38:48 -0300

    change render to component Initial

--- src/app/dashboard/components/Initial.tsx
+++ src/app/dashboard/components/Initial.tsx
@@ -1,22 +1,11 @@
 import React from 'react'
+import RecentProjects from './RecentProjects'
 
 export default function Initial() {
   return (
     <div>
         <h1>Bienvenido </h1>
-        <ul>
-            <li>Crear Proyecto Nuevo</li>
-            <li>Editar Proyecto </li>
-            <li>Eliminar Proyecto </li>
-        </ul>
-    
-        <h3>Proyectos Recientes </h3>
-        <ul>
-            <li>Portafolios </li>
-            <li>Camaras fotograafica </li>
-            <li>Estantes para locales comerciales</li>
-        </ul>
-
+        <RecentProjects/>
         <h3>Documentos Recientes </h3>
         <ul>
             <li>Portafolios </li>
