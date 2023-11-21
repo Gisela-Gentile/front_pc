3d697ab402673526776220eda325b4dc58ca23c4
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-21 20:57:15 -0300

    reemplazo ListCategories por DisplayCategories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
+import DisplayCategories from "@/components/DisplayCategories"
 import DocumentsMostViewed from "@/components/DocumentsMostViewed"
-import ListCategories from "@/components/ListCategories"
 import Presentation from "@/components/Presentation"
 import ProjectsMostViewed from "@/components/ProjectsMostViewed"
 import Search from "@/components/Search"
@@ -7,12 +7,18 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function Home() {
   return (
-    <>
+    <div className="container">
       <Presentation />
       <Search />
       <ProjectsMostViewed />
-      <ListCategories />
+      <section className="p-4">
+        <div>
+        <h2 className="py-4">Categorias</h2>
+        <DisplayCategories format={'list-button'}/>  
+        </div>
+      </section>
+      
       <DocumentsMostViewed />
-    </>
+    </div>
   )
 }
