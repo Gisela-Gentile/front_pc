85c340ab822f0afca748852d6233afed6337105a
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-21 21:16:50 -0300

    modifico componente ProjectLink para que seleccione proyecto y redirija

--- src/app/dashboard/components/ProjectLink.tsx
+++ src/app/dashboard/components/ProjectLink.tsx
@@ -1,12 +1,24 @@
+"use client"
+import { useProjects } from '@/app/context/ProjectContext';
 import { Project } from '@/app/interfaces/Project'
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-export function ProjectLink({ project }:{ project: Project },) {
-    {/*const {projectId,title, creationDate, author, category,description } = project;      */}
+export function ProjectLink({ project }:{ project: Project },) {   
+    const router = useRouter();
+    const {projectsOwner, setSelectedProject} = useProjects();
+
+    function handleClick(){
+      const finded = projectsOwner.find((actual) => (actual.projectId === project.projectId));
+      if (finded!== undefined){
+        setSelectedProject(finded);
+      }
+      router.push(`/dashboard/projects/${project.projectId}`)
+    }
     return (        
         <>
-          { project.projectId && (
-          <Link href='/dashboard/project/[projectId]'as={`/dashboard/project/${project.projectId}`}>{project.title}</Link>)}               
+          { project.projectId &&
+            <div><button onClick={handleClick}  className="">{project.title}</button></div>
+          }                       
         </>
     )
 }
