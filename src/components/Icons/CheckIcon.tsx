4b174699a1364c67f982120c1c637b3bd2e42cf6
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-21 21:25:00 -0300

    elimino componentes en deshuso

--- src/components/Icons/CheckIcon.tsx
+++
@@ -1,6 +0,0 @@
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-export default function CheckIcon() {
-    return (<FontAwesomeIcon icon={ faCheck} size='lg' style={{ color: "#31fb09"}} />)
-}
