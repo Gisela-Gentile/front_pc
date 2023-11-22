b6310e3ab1e057821db597cb6b99b90d4493d382
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-21 21:36:22 -0300

    modifico componente para reutilizar en DisplayCategories

--- src/components/ListCategories.tsx
+++ src/components/ListCategories.tsx
@@ -1,35 +1,26 @@
-import { API_URL } from '@/config/constants';
-import { Category } from '@/app/interfaces/Category';
-import styles from '@/components/ListCategories.module.css'
-async function fetchCategories() {
-  const res = await fetch(`${API_URL}/category/view/all`);
-  const data = await res.json();
-  return data;
-}
 
-async function ListCategories() {
-  const categories = await fetchCategories();
+import { Category } from '@/app/interfaces/Category';
+import styles from '@/components/DisplayCategories.module.css'
+
+
+export default function ListCategories({list,format}:{list:Category[],format:string}) {
   return (
-    <section className='p-4'>      
-      <div className='row'>
-        <div className='col-md-1'><h5>Categorías</h5></div>
-        <div className='col-md-11'>
-          <ul className={styles.defaultItemList}>
-            { 
-              categories.length > 0 ? (
-              categories.map((category: Category) => (
-                <li key={category.categoryId}>
-                  <button type="button" className="btn btn-outline-secondary btn-sm text-capitalize">{category.name}</button>
-                </li>))
-              ) : (
-              <li key={0}>No existen Categorias vigentes.</li>
-              )
+    <>
+      { 
+        list.length > 0 ? 
+        (
+          list.map((category: Category) => (
+            <li className={`${(format==='list-button')?'listButton':''}`} key={category.categoryId}>
+            { (format==='list-button') ? 
+              (<button type="button" className="btn btn-outline-secondary btn-sm text-capitalize">{category.name}</button>) : 
+              (<div className={styles.defaultLiTitle}>{category.name}</div>)
             }
-          </ul> 
-        </div>
-      </div>
-    </section>
+            </li>
+          ))
+        ):''
+      }
+    </>
   )
 }
 
-export default ListCategories;
+
