9a574818661ab45555a5f75a7f3dbd7b4e4ed947
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-21 21:08:55 -0300

    reordeno y agrego suspense para carga de datos loading en dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,42 +1,34 @@
 "use client"
 import { useRouter } from "next/navigation";
 import { useAuth } from "../context/AuthContext";
-import { useEffect, useState } from "react";
-import Nav from "./components/Nav";
+import { Suspense, useEffect, useState } from "react";
 import Initial from "./components/Initial";
 import { useProjects } from "../context/ProjectContext";
-import ProjectForm from "./components/ProjectForm";
-
+import Breadcrumbs from "./components/Breadcrumbs";
+import RecentProjects from "./components/RecentProjects";
 
 export default function DashboardPage() {
   const { token, user } = useAuth();
-  console.log(`token: ${token}`);
-  console.log(`user: ${user}`);
-  const [estado, setEstado] = useState("init");
   const router = useRouter();
-  const {projectsOwner,projectsCollaborator,loadProjectsOwner,loadProjectsCollaborator } = useProjects();
-  useEffect(() => {
-    if (!user) {
-      router.push('/')
-    }
-  }, [token,user]);
+  const {loadProjectsOwner,loadProjectsCollaborator } = useProjects();
+
+useEffect(() => {
+  console.log('recargo dashboard')  
+  if (!user) {    
+    router.push('/')
+  }    
+}, [token,user]);
   
-  useEffect(() => { loadProjectsOwner(); },[]);
-  useEffect(() => { loadProjectsCollaborator(); },[]);
-  
-  
+useEffect(() => { loadProjectsOwner()},[]);
+useEffect(() => { loadProjectsCollaborator()},[]);
+
   return (
-    <div className="container">
-    <div className="row">
-        <div id="sidebar" className="col-md-3" >
-          <Nav />
-        </div>
-        <div id="content" className="col-md-9" style={{ ['borderLeft' as any]: "1px solid #ccc" }}>
-        
-        {estado ==="init" ? ( <Initial/>  ):''}
-        <ProjectForm/>
-        </div>
-      </div> 
-    </div>
+    <section className="">
+      <Breadcrumbs breadcrumbs={[{ label: 'Inicio', href: '/dashboard',active: true, },]}/>
+      <hr/>        
+        <Suspense fallback={<div>Loading...</div>}>
+          <RecentProjects/>
+        </Suspense>
+    </section>    
   );
 }
