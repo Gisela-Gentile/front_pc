72904d9398401564553ff6417dc99014e2880ac5
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-21 21:06:21 -0300

    modifico layout para dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,21 @@
 import { ProjectsProvider } from "../context/ProjectContext";
+import Nav from "./components/Nav";
 
 export default function DashboardLayout({ children, }: { children: React.ReactNode }) {
-    return (   
-        <ProjectsProvider> 
-            {children}
-        </ProjectsProvider>  
-    )
+
+
+    return (  
+        <div className="container-fluid">
+            <div className="row">
+                <div id="sidebar" className="mt-3" style={{['width' as any]: "250px",['minHeight' as any]:"700px",['position' as any]:"absolute",}}>
+                   <Nav/>               
+                </div>
+        <div id="content" style={{ ['borderLeft' as any]: "1px solid #ccc",['minHeight' as any]:"700px",['paddingLeft' as any]:'260px',}}>   
+            <ProjectsProvider>
+                {children}
+            </ProjectsProvider>
+        </div>
+      </div> 
+    </div>
+    );
 }
