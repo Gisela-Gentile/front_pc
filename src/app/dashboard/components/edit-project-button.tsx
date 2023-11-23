5d139ec8786a1f86618c1c1b4cb4815cdffe5984
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-23 02:09:59 -0300

    limpio codigo

--- src/app/dashboard/components/edit-project-button.tsx
+++ src/app/dashboard/components/edit-project-button.tsx
@@ -5,23 +5,21 @@ import { useParams, useRouter } from "next/navigation";
 export default function EditProjectButton() {
   const router = useRouter();
   const {id} = useParams();
-  const {projectsOwner, setSelectedProject} = useProjects();
-
-    
+  const {projectsOwner,setSelectedProject} = useProjects();   
+  
   function handleClick(){
     const finded = projectsOwner.find((project) => (project.projectId === Number(id)));
     if (finded!== undefined){
       setSelectedProject(finded);
     }
     router.push(`/dashboard/projects/${id}/edit`)
-  }
-  
-  return (
-    <button
-      onClick={handleClick}
-      className="rounded border bg-black px-4 py-1 text-center text-white m-4"
-    >
-      Editar Proyecto
-    </button>
+  }  
+  return (  
+      <button
+        onClick={handleClick}
+        className="rounded border bg-black px-4 py-1 text-center text-white m-4"
+      >
+        Editar Proyecto
+      </button>
   );
 }
