ed6e9b6b6572c1c4767b3e714c21318228ce9edc
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-23 18:57:45 -0300

    add new component ViewTableDocumentsLinkItem to use in table

---
+++ src/app/dashboard/components/ViewTableDocumentsLinkItem.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { useProjects } from '@/app/context/ProjectContext';
+import { ItemLastDocument } from '@/app/interfaces/Document';
+import CollaboratorAutorCard from '@/components/CollaboratorAutorCard';
+import { capitalize } from '@/lib/utils';
+import { useRouter } from 'next/navigation';
+import { FaFile } from "react-icons/fa";
+
+export function ViewTableDocumentsLinkItem({ itemLastDocument }:{ itemLastDocument: ItemLastDocument },) {   
+    const router = useRouter();
+    const {selectedProject} = useProjects();
+
+    function handleClick(){
+      router.push(`/dashboard/projects/${selectedProject?.projectId}/document/${itemLastDocument.documentId}`)
+    }
+    return (        
+      <>
+      { itemLastDocument.documentId && (  
+        <tr key={itemLastDocument.documentId} onClick={handleClick}>
+          <td><FaFile/>{' '}{capitalize(itemLastDocument.title)}</td>
+          <td>{new Date(itemLastDocument.creationDate).toLocaleDateString()}</td>
+          <td><CollaboratorAutorCard id={itemLastDocument.authorColDocument} /></td>
+          <td>{new Date(itemLastDocument.creationDateHistory).toLocaleDateString()}</td>
+          <td><CollaboratorAutorCard id={itemLastDocument.authorColHistory} /></td>        
+      </tr>)
+      }                       
+      </>
+    )
+}
