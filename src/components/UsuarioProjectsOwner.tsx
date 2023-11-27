1ebc0122e11d5b994d7d6d801af2046bfbe5d3cf
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-27 13:36:09 -0300

    transform components to use client

--- src/components/UsuarioProjectsOwner.tsx
+++ src/components/UsuarioProjectsOwner.tsx
@@ -1,15 +1,23 @@
 import { Project } from "@/app/interfaces/Project";
 import { API_URL } from "@/config/constants";
 import { ProjectCard } from "./ProjectCard";
+import { useEffect, useState } from "react";
 
-async function fetchUsuarioProjectsOwner(id:number):Promise<Project[]> {
-  const res = await fetch(`${API_URL}/project/user/${id}/owner-projects`,{cache: 'no-store'});
-  const data = await res.json();
-  return data.data;
-}
-
-export default async function UsuarioProjectsOwner({id}:{id: number}) {
-  const projects = await fetchUsuarioProjectsOwner(id);
+export default function UsuarioProjectsOwner({id}:{id: number}) {
+  const [projects, setProjects] = useState<Project[]>();
+  useEffect(() => {
+    const fetchUsuarioProjectsOwner = async () => {
+        try {
+          const res = await fetch(`${API_URL}/project/user/${id}/owner-projects`,{cache: 'no-store'});
+          const data = await res.json();
+          setProjects(data.data);
+        } catch (error) {
+            console.error('Error recuperando proyectos propio del Usuario', error);
+        }
+    };
+    fetchUsuarioProjectsOwner();
+  }, []);
+    
   return (
     <section>
       <div className="container">
@@ -18,7 +26,7 @@ export default async function UsuarioProjectsOwner({id}:{id: number}) {
             <h2 className="text-center text-primary pb-3 pt-3">Proyectos Propios</h2>
             </div>
             <div className="row">
-            { projects.length > 0 ? (
+            { projects && projects.length > 0 ? (
               projects.map((project:Project) => (
               <div className='col-md-4 p-2' key={project.projectId}>
                   <ProjectCard project={project} />
