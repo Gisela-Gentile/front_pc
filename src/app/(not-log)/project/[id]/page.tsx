7ea24220e79db43a94cb1b75dbd9e0e70436d426
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-27 20:15:50 -0300

    add use client and rewrite fetch in useEfect

--- src/app/(not-log)/project/[id]/page.tsx
+++ src/app/(not-log)/project/[id]/page.tsx
@@ -3,22 +3,33 @@ import Breadcrumbs from "@/app/dashboard/components/Breadcrumbs";
 import { ProjectComplete} from "@/app/interfaces/Project";
 import ProjectViewComplete from "@/components/ProjectViewComplete";
 import { API_URL } from "@/config/constants";
+import { useEffect, useState } from "react";
 
-async function fetchProjectComplete(id: number): Promise<ProjectComplete> {
-  const res = await fetch(`${API_URL}/project/${id}/view/complete`);
-  const data = await res.json();
-  return data;
-}
-
-export default async function ProjectPage({ params: { id }, }: { params: { id: string } }) {
-  const projectComplete = await fetchProjectComplete(Number(id));
+export default function ProjectPage({ params: { id }, }: { params: { id: string } }) {
+  
+  const [projectComplete, setProjectComplete] = useState<ProjectComplete>();
+  
+  useEffect(() => {
+    const fetchProjectComplete = async () => {
+        try {
+          const res = await fetch(`${API_URL}/project/${id}/view/complete`);
+          const data = await res.json();
+          setProjectComplete(data);
+        } catch (error) {
+            console.error('Error recuperando proyecto ', error);
+        }
+    };
+    fetchProjectComplete();
+  },[]); 
+   
+  
   return (
     <div className="container"> 
      <Breadcrumbs breadcrumbs={[{ label: 'Inicio', href: '/', },]}/>
       <hr/>   
       <div className="row">
         <div className="offset-2 col-8">        
-          <ProjectViewComplete projectComplete={projectComplete} />    
+         { projectComplete && <ProjectViewComplete projectComplete={projectComplete} />}    
           </div>
       </div>
     </div>
