6f75f5310df9153a84f40b5225428cfd2c897806
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-27 05:03:00 -0300

    restructuring component and data

--- src/app/dashboard/components/RecentProjects.tsx
+++ src/app/dashboard/components/RecentProjects.tsx
@@ -1,10 +1,20 @@
 
 import { useProjects } from '@/app/context/ProjectContext';
 import ViewListProjects from './ViewListProjects';
+import { useEffect } from 'react';
 
 export default function RecentProjects() {
-  const {projectsOwner,projectsCollaborator } = useProjects();
-        
+          
+  const { projectsOwner,
+          projectsCollaborator,
+          loadProjectsOwner,
+          loadProjectsCollaborator
+  } = useProjects();
+
+  console.log("Recargo recent projects!!!")
+  useEffect(() => { loadProjectsOwner()},[]);
+  useEffect(() => { loadProjectsCollaborator()},[]);
+
   return (
     <>
     <ViewListProjects title={`Mis últimos Proyectos`} list={projectsOwner} cantidad={4} msgResult={`No posee Proyectos recientes.`} viewMore={true}/> 
