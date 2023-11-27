8a02fbb29a5b43b3efaa3a63485b4a4978ce3d5f
AndresLeoFernandez <andres.fernandez@municipiodetandil.edu.ar>
2023-11-27 20:13:58 -0300

    add use client

--- src/components/ItemListDocument.tsx
+++ src/components/ItemListDocument.tsx
@@ -1,15 +1,22 @@
-
+"use client"
 import { ItemLastDocument } from '@/app/interfaces/Document';
 import { capitalize } from '@/lib/utils';
-import Link from 'next/link'; 
+import { useRouter } from 'next/navigation';
+import router from 'next/router';
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
 
 export default function ItemListDocument({document}:{document:ItemLastDocument}) {
+  const router = useRouter();
+
+  function handleClick(){
+    router.push(`/document/${document.documentId}`)
+  }
+
   return (
-    <>      
+    <tr onClick={handleClick} className="">
       <td>{new Date(document.creationDate).toLocaleDateString()}</td> 
       <td>{capitalize(document.title)}</td>
-      <td><Link href={`/document/${document.documentId}`} title='Ir al documento'><FaRegArrowAltCircleRight/></Link></td>
-    </>
+      <td><FaRegArrowAltCircleRight/></td>
+    </tr>
   )
 }
