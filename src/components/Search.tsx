a9370952b8822ed8a1c7c22cacfb2f69c7284adb
Gisela-Gentile <gisela.gentile@municipiodetandil.edu.ar>
2023-11-28 15:07:17 -0300

    Add changes in search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,7 +2,8 @@
 import { createUrl } from '@/lib/utils';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { HiOutlineSearch } from "react-icons/hi";
-
+import Image from 'next/image';
+import Link from 'next/link';
 export default function Search() {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -25,7 +26,20 @@ export default function Search() {
 
   return (
     <section>
-      <div className="row justify-content-center m-5">
+      <div className='row justify-content-center m-5'>
+      <div className="col-12 align-self-center text-center">
+      <Link href={'/search'}>{<Image
+      src="/assets/buscar.gif"
+      alt="presentation"
+      width={20}
+      height={20}
+            sizes="(max-width: 768px) 100vw,(max-width:1200 px) 50vw,33vw"
+      style={{ height: '10%', width: '10%', }} priority/> }
+      
+</Link>
+</div>
+</div>
+      {/* <div className="row justify-content-center m-5">
       <div className="col-6 align-self-center">
       <form onSubmit={onSubmit}>
         <input
@@ -41,7 +55,7 @@ export default function Search() {
         <HiOutlineSearch/></span>
       </form>
       </div>
-      </div>
+      </div> */}
     </section>
   );
 }
